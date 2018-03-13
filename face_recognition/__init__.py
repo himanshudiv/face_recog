59839773baae3ed29dfaad6c1746277833f9328b
Himanshu Srivastava <srivastavahimanshu24@gmail.com>
2018-03-13 09:48:25 +0530

    Update __init__.py

--- face_recognition/__init__.py
+++ face_recognition/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__author__ = """Adam Geitgey"""
-__email__ = 'ageitgey@gmail.com'
+__author__ = """Himanshu Srivastava"""
+__email__ = 'himsriv05@gmail.com'
 __version__ = '1.2.2'
 
 from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
